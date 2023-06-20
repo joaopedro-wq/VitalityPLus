f117d6e10b26aa257d5c2940fa96b55e398bced7
João Pedro  Bandeira <joao.bandeiraoliveira@gmail.com>
2023-06-20 20:49:54 -0300

    Create HomePage.js

---
+++ src/components/pages/HomePage.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import '../css/HomePage.css'
+import { Link } from 'react-router-dom';
+function HomePage() {
+  return (
+    <div>
+      <section id="banner" className="banner section">
+        <h1>Bem-vindo à plataforma de saúde e bem-estar!</h1>
+        <p>Aqui você encontra tudo o que precisa para cuidar da sua saúde de forma personalizada.</p>
+        <a href="#" className="cta-button">Comece Agora</a>
+      </section>
+
+      <section id="about" className="about ">
+        <h2>Sobre Nós</h2>
+        <p>Algumas informações sobre a Vitality PLUS.</p>
+      </section>
+
+      <section id="services" className="services">
+        <h2>Serviços</h2>
+        <p>Descrição dos serviços que sua plataforma oferece aos usuários.</p>
+        <Link to="/services" className="services-button">Veja nossos serviços</Link>
+      </section>
+
+      <section id="blog" className="blog">
+        <h2>Blog</h2>
+        <p>Últimas notícias e artigos relacionados à saúde e bem-estar.</p>
+        <Link to="/blog" className="services-button">Veja o Blog</Link>
+      </section>
+
+      <section id="contact" className="contact">
+        <h2>Contato</h2>
+        <p>Entre em contato conosco para obter mais informações ou tirar suas dúvidas.</p>
+      </section>
+    </div>
+  );
+}
+
+export default HomePage;
+
+
+
+
