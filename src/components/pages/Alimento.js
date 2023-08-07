7d3ddb401c159f309e07c672283d5e0a6eecdf2e
joaopedro-wq <joao.bandeiraoliveira@gmail.com>
2023-08-07 00:40:52 -0300

    update

--- src/components/pages/Alimento.js
+++ src/components/pages/Alimento.js
@@ -8,18 +8,70 @@ function Alimentos() {
  
   const [alimentos] = useState(alimentosData);
   const loading = false; 
+  const [searchTerm, setSearchTerm] = useState('');
+  const [filteredAlimentos, setFilteredAlimentos] = useState(alimentosData);
+  const [searchGroup, setSearchGroup] = useState('')
+  const [selectedGroup, setSelectedGroup] = useState(''); 
+  
+  const handleSearch = (searchTerm) => {
+    setSearchTerm(searchTerm);
+  
+    const filtered = alimentosData.filter(alimento =>
+      alimento['Descrição do Alimento'].toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setFilteredAlimentos(filtered);
+  };
+
+  const handleGroupSearch = (searchGroup) => {
+    setSearchGroup(searchGroup);
+  
+    const filtered = alimentosData.filter(alimento =>
+      alimento['Grupo'].toLowerCase().includes(searchGroup.toLowerCase())
+    );
+    setFilteredAlimentos(filtered);
+  };
+  
+  
+
   return (
     <div className="alimentos-container" style={{ marginBottom: '50px' }}>
       <h2>Alimentos</h2>
       <p>Unidade: 100(g)</p>
-      {/* Seu código de filtro e pesquisa aqui */}
+      <div className="filter-container">
+        <div className="search-container">
+          <input
+            type="text"
+            placeholder="Pesquisar alimentos"
+            value={searchTerm}
+            onChange={(e) => handleSearch(e.target.value)}
+          />
+        </div>
+        <div className="group-filter">
+          <select
+            value={selectedGroup}
+            onChange={(e) => {
+              setSelectedGroup(e.target.value);
+              handleGroupSearch(e.target.value);
+            }}
+          >
+            <option value="">Selecione um grupo</option>
+            {Array.from(
+              new Set(alimentosData.map((alimento) => alimento['Grupo']))
+            ).map((grupo) => (
+              <option key={grupo} value={grupo}>
+                {grupo}
+              </option>
+            ))}
+          </select>
+        </div>
+      </div>
+
       {loading ? (
         <p>Carregando alimentos...</p>
       ) : (
         <table className="alimentos-table">
           <thead>
             <tr>
-             <th>Grupo</th>
               <th>Nome</th>
               <th>Calorias</th>
               <th>Proteínas</th>
@@ -29,9 +81,9 @@ function Alimentos() {
             </tr>
           </thead>
           <tbody>
-            {alimentos.map((alimento) => (
+            {filteredAlimentos.map((alimento) => (
               <tr key={alimento['Número']}>
-                <td>{alimento['Grupo']}</td>
+                
                 <td>{alimento['Descrição do Alimento']}</td>
                 <td>{alimento['Energia(kcal)']}</td>
                 <td>{alimento['Proteína(g)']}</td>
--- src/components/pages/RegistroNutricional.js
+++ src/components/pages/RegistroNutricional.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import '../css/RegistroNutricional.css';
 import Select from 'react-select';
 import alimentosData from '../../services/taco.json'; 
+import { format } from 'date-fns';
 
 function RegistroNutricional() {
   const [data, setData] = useState('');
@@ -20,7 +21,7 @@ function RegistroNutricional() {
       proteinas: 0,
       carboidratos: 0,
       gorduras: 0,
-      selectedFood: null
+      selectedFood: null,
     }
   ]);
 
@@ -59,24 +60,33 @@ function RegistroNutricional() {
       calorias: 0,
       proteinas: 0,
       carboidratos: 0,
-      gorduras: 0
-    };
+      gorduras: 0,
+      selectedFood: null,
+  };
+    
 
     setAlimentos([...alimentos, novoAlimento]);
   };
   const handleAlimentoChange = async (index, field, value) => {
     const novosAlimentos = [...alimentos];
     novosAlimentos[index][field] = value;
-    setAlimentos(novosAlimentos);
+  
+    // Defina selectedFood para o primeiro alimento, se ainda não estiver definido
+    if (!novosAlimentos[index].selectedFood && value) {
+      novosAlimentos[index].selectedFood = { value, label: value };
+    }
+  
+    setAlimentos(novosAlimentos); 
 
-    if (field === 'nome') {
+    if (field === 'nome' && value) {
+      novosAlimentos[index].selectedFood = { value, label: value };
       setSelectedFood({ value, label: value });
       await obterInformacoesNutricionais(value, index);
     } else if (field === 'quantidade') {
       const quantidade = parseFloat(value); 
       const alimentoInfo = alimentosData.find(
-        alimento => alimento['Descrição do Alimento'] === selectedFood.value
-      );
+        alimento => selectedFood && alimento['Descrição do Alimento'] === selectedFood.value
+    );
   
       if (alimentoInfo) {
         const descricao = (alimentoInfo['Descrição do Alimento']);
@@ -133,8 +143,16 @@ function RegistroNutricional() {
   
 
   const handleReset = () => {
+    setTotals({
+      calorias: 0,
+      proteinas: 0,
+      carboidratos: 0,
+      gorduras: 0,
+    });
+  
     setRegistroDiario([]);
   };
+  
   
   const obterInformacoesNutricionais = async (alimento, index) => {
     const alimentoInfo = alimentosData.find(item => item['Descrição do Alimento'] === alimento);
@@ -182,7 +200,7 @@ function RegistroNutricional() {
 
   return (
     <div className="registro-nutricional-container" style={{ marginBottom: '80px' }}>
-    <h2>Registro Nutricional</h2>
+    <h2>Registro Nutricional Diário</h2>
     <form className="registro-form" onSubmit={handleSubmit}>
       <div className="input-container">
         <label htmlFor="data">Data:</label>
@@ -190,34 +208,40 @@ function RegistroNutricional() {
       </div>
       <div className="input-container">
         <label htmlFor="refeicao">Refeição:</label>
-        <input type="text" id="refeicao" value={refeicao} onChange={(e) => setRefeicao(e.target.value)} />
+        <input placeholder='Nome da refeição' required type="text" id="refeicao" value={refeicao} onChange={(e) => setRefeicao(e.target.value)} />
       </div>
       {alimentos.map((alimento, index) => (
   <div className="alimento-container" key={index}>
-    <label>Alimento:</label>
+    <label className='labelQTD'>Alimento:</label>
     <Select
       options={foodOptions}
-      value={alimento.selectedFood}  // Altere esta linha
+      value={alimento.selectedFood}  
       onChange={value => handleAlimentoChange(index, 'nome', value.value)}  // Altere esta linha
       placeholder="Selecione um alimento..."
     />
 
-    <label>Quantidade (g):</label>
+    <label className='labelQTD'>Quantidade (g):</label>
     <input
+      required
+      className='qtdInput'
       type="number"
-      value={alimento.quantidade}
+      value={alimento.quantidade === 0 ? '' : alimento.quantidade}
       onChange={(e) => handleAlimentoChange(index, 'quantidade', e.target.value)}
+      placeholder='Quantidade do alimento'  
     />
  
   </div>
 ))}
-      <button type="button" onClick={handleAdicionarAlimento}>
+  <div className='buttons-container'>
+
+      <button className='adicionar-button'  type="button" onClick={handleAdicionarAlimento}>
         Adicionar Alimento
       </button>
-      <button  type="button" onClick={handleReset}>
+      <button className='reiniciar-button' type="button" onClick={handleReset}>
         Reiniciar Registro Diário
       </button>
       <button className='salvar' type="submit">Salvar</button>
+  </div>
     </form>
   
     <div className="registro-diario">
@@ -227,16 +251,17 @@ function RegistroNutricional() {
         {registroDiario.map((registro, index) => (
           <li key={index}>
             <h4>Refeição: {registro.refeicao}</h4>
-            <p className='dataDiaria' >Data: {registro.data}</p>
+            <p className='dataDiaria'>Data: {format(new Date(registro.data), 'dd/MM/yyyy')}</p>
+
             <ul className="alimentos-list">
               {registro.alimentos.map((alimento, index) => (
                 <li key={index}>
                   <span>Alimento: {alimento.descricao}</span>
-                  <span>Quantidade: {alimento.quantidade.toFixed(2)} (g)</span>
+                  <span>Quantidade: {alimento.quantidade.toFixed(2)}g</span>
                   <span>Calorias: {alimento.calorias.toFixed(2)} kcal</span>
-                  <span>Proteínas: {alimento.proteinas.toFixed(2)}</span>
-                  <span>Carboidratos: {alimento.carboidratos.toFixed(2)}</span>
-                  <span>Gorduras: {alimento.gorduras.toFixed(2)}</span>
+                  <span>Proteínas: {alimento.proteinas.toFixed(2)}g</span>
+                  <span>Carboidratos: {alimento.carboidratos.toFixed(2)}g</span>
+                  <span>Gorduras: {alimento.gorduras.toFixed(2)}g</span>
                 </li>
               ))}
             </ul>
@@ -255,6 +280,7 @@ function RegistroNutricional() {
       <p>Carboidratos Totais: {totals.carboidratos.toFixed(2)}g</p>
       <p>Gorduras Totais: {totals.gorduras.toFixed(2)}g</p>
     </div>
+
 
   </div>
   
